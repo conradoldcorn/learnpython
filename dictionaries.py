43e7634b6ea9150940b4fb6da0f31ca1ba9b4f59
conrad <coldcorn@gmail.com>
2017-07-23 10:34:36 +0100

    working

--- dictionaries.py
+++ dictionaries.py
@@ -1,14 +1,20 @@
-stock_dict = {'hay': 3, 'food': 1, 'banana_chips': 4}
+stock_dict = {'hay': 3, 'food': 1, 'banana_chips': 0}
 
 # Iterate through the stock, if the value of the item is 1 or less, print 'Order more ITEM'
 in_stock = []
+out_stock = []
+
 
 for key, val in stock_dict.items():
         if val <= 1:
-            print("Order more: {}".format(key))
+            out_stock.append(key)
 
         else:
 
             in_stock.append(key)
 
-print("You don't need to order {}".format(in_stock))
+formatted_out_stock = ', '.join(out_stock)
+formatted_in_stock = ', '.join(in_stock)
+
+print("Order more: {}".format(formatted_out_stock))
+print("You don't need to order {}".format(formatted_in_stock))
