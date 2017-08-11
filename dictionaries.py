3a5840041cdd9fe90998af5dc0446820cc29e3b1
conrad <coldcorn@gmail.com>
2017-08-11 15:43:32 +0100

    adding random functionality

--- dictionaries.py
+++ dictionaries.py
@@ -1,9 +1,13 @@
-stock_dict = {'hay': 3, 'food': 1, 'banana_chips': 0}
-
 # Iterate through the stock, if the value of the item is 1 or less, print 'Order more ITEM'
+import random
+
+stock_dict = {'hay': random.randint(0,10), 'food': random.randint(0,10), 'banana_chips': random.randint(0,10)}
+
+#random.choice(stock_dict.items())
+print(stock_dict)
+
 in_stock = []
 out_stock = []
-
 
 for key, val in stock_dict.items():
         if val <= 1:
@@ -16,5 +20,6 @@ for key, val in stock_dict.items():
 formatted_out_stock = ', '.join(out_stock)
 formatted_in_stock = ', '.join(in_stock)
 
+#if formatted_out_stock.items(<= 1):
 print("Order more: {}".format(formatted_out_stock))
 print("You don't need to order {}".format(formatted_in_stock))
